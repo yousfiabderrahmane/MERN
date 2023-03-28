9caf359427aa0a63cd676fe2ac7174272992d99b
Abderrahmane Yousfi <bdrrhmnyousfi@gmail.com>
2023-03-28 10:11:57 +0000

    feat; mongoDB & mongoose setup + connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const workoutRoutes = require("./routes/workouts");
 
+// env variables
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 // express app
 const app = express();
-const PORT = process.env.PORT;
 
 // middleware
 app.use(express.json()); //it looks for the body in the post request and parses it so we can acces it later on from the req
@@ -16,7 +19,13 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes); //when we go to this path use this router
 
-// listen for requests
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+//connect to db
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    // listen for requests after connection to db
+    app.listen(PORT, () => {
+      console.log(`Connected to db, Now listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
