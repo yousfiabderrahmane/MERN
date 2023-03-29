40dabd5448982ee7c66c29b8bc88fc594a206dc6
Abderrahmane Yousfi <bdrrhmnyousfi@gmail.com>
2023-03-29 16:24:55 +0000

    feat: Signing up + hashing password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
@@ -16,4 +18,23 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+// static signup method
+
+userSchema.statics.signup = async (email, password) => {
+  const exists = await User.findOne({ email }); //this refers to this model
+
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await User.create({ email, password: hash });
+
+  return user;
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
